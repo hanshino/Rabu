bb44dc414f3fc4c1ff8a8b9a36a0869089cc5d48
hanshino <c0952199517@gmail.com>
2021-09-21 17:26:59 +0800

    未載入語言配置前，進行畫面loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "./styles/loader.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from "./containers/Navbar";
 import Dashboard from "./pages/Dashboard";
+import NewRoom from "./pages/NewRoom";
 import { useSelector } from "react-redux";
 import { cyan } from "@mui/material/colors";
+import Loader from "./components/Loader";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
+  const { i18n } = useTranslation();
   const appTheme = useSelector(state => state.theme);
   const darkMode = appTheme.theme === "dark";
 
@@ -24,12 +29,21 @@ const App = () => {
     [darkMode]
   );
 
+  if (!i18n.language) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Loader />
+      </ThemeProvider>
+    );
+  }
+
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <Navbar>
           <Switch>
             <Route path="/dashboard" component={Dashboard} />
+            <Route path="/room/create" component={NewRoom} />
           </Switch>
         </Navbar>
       </ThemeProvider>
